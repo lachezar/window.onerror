f2c27025e10230cdd1ec86739259ae13a384e94d
lucho870601 <lucho870601@gmail.com>
2013-10-21 19:15:50 +0200

    Added onerror handler and busterjs test for it

---
+++ onerror.js
@@ -0,0 +1,19 @@
+initErrorHandler = function(errorUrl, staticHost) {
+  window.onerror = function(message, scriptUrl, line) {
+    var regex = new RegExp("^https?://" + staticHost);
+    if (regex.test(scriptUrl) && scriptUrl.indexOf('.js') > -1) {
+      // if the script origin is ours
+      // send to server for post mortem analysis
+      $.post(
+        errorUrl,
+        {
+          location: location.href, 
+          message: message, 
+          scriptUrl: scriptUrl, 
+          line: line, 
+          useragent: navigator.userAgent
+        }
+      );
+    }
+  };
+}
--- test/onerror-test.js
+++ test/onerror-test.js
@@ -1,42 +1,39 @@
+var assert = buster.referee.assert;
+
 buster.testCase("Call onerror", {
 
   setUp: function () {
     $ = {};
     $.post = this.spy();
+
+    initErrorHandler("/error", "localhost");
   },
     
   "exception within the system": function () {
-    
-    setTimeout(function() {
-      throw new Exception("test");
-    }, 100);
-    
-    setTimeout(function() {
-      assert.called($.post);
-    }, 200);
-  }
+    window.onerror('error message', 'http://localhost/test.js', 11);
+    assert.isTrue($.post.called);
+  },
   
-  "error within the system": function () {
+  "error within the system": function (done) {
     
     setTimeout(function() {
-      test.test();
+      // throw some real-life exception
+      not_defined.not_defined();
+    }, 10);
+    
+    setTimeout(function() {
+      assert.isTrue($.post.called);
+      done();
     }, 100);
-    
-    setTimeout(function() {
-      assert.called($.post);
-    }, 200);
-  }
+  },
   
   "error outside the system": function() {
-    
-    setTimeout(function() {
-      var script = document.createElement('script');
-      script.src = "http://google.com";
-      document.body.appendChild(script);
-    }, 100);
-    
-    setTimeout(function() {
-      assert.called($.post);
-    }, 200);
+    window.onerror('error message', 'http://example.com/test.js', 11);
+    assert.isFalse($.post.called);
+  },
+  
+  "error from unknown source": function() {
+    window.onerror('error message', 'http://localhost/foo', 11);
+    assert.isFalse($.post.called);
   }
 });
